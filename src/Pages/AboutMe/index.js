cb284bb25bb77b1d15967e306294fffe8abcb10b
dyoder838 <dyoder838@gmail.com>
2021-03-10 15:19:40 -0800

    forgot to save

---
+++ src/Pages/AboutMe/index.js
@@ -0,0 +1,21 @@
+import React from "react";
+import Grid from '@material-ui/core/Grid';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+
+
+}));
+
+export default function MainPage() {
+
+    const classes = useStyles();
+
+    return (
+
+        <Grid>
+
+        </Grid>
+       
+    );
+};
