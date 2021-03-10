d49111af2b6f984fc8ed06b6de9fda4707d20400
dyoder838 <dyoder838@gmail.com>
2021-03-10 15:13:49 -0800

    added router in app.js to pages

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import About from '../src/Pages/AboutMe'
+import Projects from '../src/Pages/Projects'
 
 function App() {
   return (
-    <div>
-      
-    </div>
+    <Router>/
+            
+            <Switch>
+
+                <Route exact path="/" component={About}/>
+                <Route exact path="/projects" component={Projects}/>
+            
+            </Switch>
+            
+        </Router>
   );
 }
 
